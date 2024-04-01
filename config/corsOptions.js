6579025eaddd4e3fbcf0f8b692a6e6c90230ca8a
nobody543210 <151631368+nobody543210@users.noreply.github.com>
2024-04-01 02:41:32 +0000

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'https://zodiacsignsapp.onrender.com',
-    'http://localhost:3000/'
+    'https://localhost:3000/',
+    'https://localhost:3000/',
+    'https://127.0.0.1:3000/'
 ];
 
 const corsOptions = {
